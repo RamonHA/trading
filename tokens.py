d791577784b6de0d3c9adc8abc16b7f62e45c26e
RamonHA <ramon1001@live.com.mx>
2021-10-03 21:23:37 -0500

    Add gitignore

--- tokens.py
+++ tokens.py
@@ -1,11 +1,15 @@
 # path for data to work with
-ROUTE = ""
+ROUTE = r"C:\Users\ramon\Documents\Trading"
 
 ## Brokers tokens
 
 # Binance
+BINANCE_API_KEY = 'CEBTaNQc0rSTtZcirBfD7LOnCQ5lidBhlXo60qIOuyBxnNdIx0LlPeYuy3Edh7PF'
+BINANCE_API_SECRETE = 'rqazicO9dGJYmDhopq8bOy8JguOcCCrEuFWkX4TsX8UR94SnAkFYzH5eBP2pIBrT'
+
 
 # Bitso
+BITSO_AV_KEY = 'TXFOWRWPMIF5LT34'
 
 # Dictionary of comissions
 COMISION = {
