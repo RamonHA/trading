329ce1635dbb6819843ca274dd8a91c13693e5d1
RamonHA <ramon1001@live.com.mx>
2021-10-03 21:24:21 -0500

    Blank tokens

--- tokens.py
+++ tokens.py
@@ -1,15 +1,12 @@
 # path for data to work with
-ROUTE = r"C:\Users\ramon\Documents\Trading"
+ROUTE = ""
 
 ## Brokers tokens
 
 # Binance
-BINANCE_API_KEY = 'CEBTaNQc0rSTtZcirBfD7LOnCQ5lidBhlXo60qIOuyBxnNdIx0LlPeYuy3Edh7PF'
-BINANCE_API_SECRETE = 'rqazicO9dGJYmDhopq8bOy8JguOcCCrEuFWkX4TsX8UR94SnAkFYzH5eBP2pIBrT'
-
 
 # Bitso
-BITSO_AV_KEY = 'TXFOWRWPMIF5LT34'
+
 
 # Dictionary of comissions
 COMISION = {
