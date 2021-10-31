074b903d6c904f40c5ca2d4d7eeee3fcee2fc9e5
RamonHA <ramon1001@live.com.mx>
2021-10-31 10:32:05 -0600

    NN

--- neuralnetworks.py
+++ neuralnetworks.py
@@ -1,12 +1,44 @@
 
-
+from sklearn.preprocessing import MinMaxScaler
 
 class NN():
     def __init__(
-            self
+            self,
+            stationarity = True,
+            ensure_stationarity = False,
+            **kwargs
         ):
-        raise NotImplementedError
+        """  
+            stationarity (bool): Si transformar la data a estacionaria con una PRIMERA diferencia
+                Default: True
 
+            ensure_stationarity (bool): Hacer las transformaciones necesarias para ASEGURAR
+                                        estacionariedad
+                Defualt: False
+
+        """
+
+    def scale(self, train, test, scaler = MinMaxScaler(feature_range=(-1, 1)) ):
+        
+        assert callable(scaler), "scaler tiene que ser una funcion callable, se entego {}".format(type(scaler))
+
+        scaler = scaler.fit(train)
+        train_scaled = scaler.transform(train)
+        test_scaled = scaler.transform(test)
+
+        self.scaler = scaler
+
+        return train_scaled, test_scaled
+
+
+    def fit(self):
+        raise NotImplementedError
+    
+    def predict(self):
+        raise NotImplementedError
+    
+    def run(self):
+        raise NotImplementedError
 
 
 class LSTM(NN):
